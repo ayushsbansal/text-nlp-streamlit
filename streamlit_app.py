e7def9b1d881666a46d943d0a99c7948893719cd
Ayush Bansal <47652982+ayushsbansal@users.noreply.github.com>
2026-05-28 15:26:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,11 @@ st.set_page_config(
     layout="wide"
 )
 
-SERVER_URL = st.secrets.get("SERVER_URL", "http://localhost:8000")
+SERVER_URL = st.secrets.get("SERVER_URL")
 
 def main():
 	st.title("Text-to-SQL NLP Application")
+	st.write("Backend hosted at: ${SERVER_URL}")
 	st.write("Enter a natural language query to generate SQL code.")
 
 	user_input = st.text_input("Natural Language Query", "Find all employees in Singapore")
